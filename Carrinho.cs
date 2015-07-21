53653b81e7bdf9dfa8c59bceb2fb4c88f88d5638
Gabriel <developer.gabriel@gmail.com>
2015-07-21 15:58:02 -0300

    corrigindo estrutura

---
+++ Carrinho.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Loja.Models
+{
+    public class Carrinho
+    {
+        public List<Produto> Produtos { get; set; }
+        public string Endereco { get; set; }
+        public long Id { get; set; }
+
+        public Carrinho()
+        {
+            this.Produtos = new List<Produto>();
+        }
+
+        public void Adiciona(Produto produto)
+        {
+            this.Produtos.Add(produto);
+        }
+
+        public void Remove(long id)
+        {
+            Produto produto = Produtos.FirstOrDefault(p => p.Id == id);
+
+            Produtos.Remove(produto);
+        }
+
+        public void Troca(Produto produto)
+        {
+            Remove(produto.Id);
+            Adiciona(produto);
+        }
+
+        public void TrocaEndereco(string endereco)
+        {
+            this.Endereco = endereco;
+        }
+		
+		public void TrocaQuantidade(Produto produto)
+        {
+            Produto produtoCarregado = Produtos.FirstOrDefault(p => p.Id == produto.Id);
+
+            produtoCarregado.Quantidade = produto.Quantidade;
+        }
+    }
+}
---
+++ Produto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using a = System.CannotUnloadAppDomainException;
+
+namespace Loja.Models
+{
+    public class Produto
+    {
+        public long Id { get; set; }
+        public double Preco { get; set; }
+        public string Nome { get; set; }
+        public int Quantidade { get; set; }
+
+        public Produto()
+        {
+
+        }
+
+        public Produto(long id, string nome, double preco, int quantidade)
+        {
+            this.Id = id;
+            this.Nome = nome;
+            this.Preco = preco;
+            this.Quantidade = quantidade;
+        }
+    }
+}
